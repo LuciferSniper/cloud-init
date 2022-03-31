de6a54b02a519f2fd55ec860d8ac2b57c92234d2
LuciferSniper <94542546+LuciferSniper@users.noreply.github.com>
2022-04-01 01:03:56 +0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://LuciferSniper:$1@github.com/LuciferSniper/node.git
+git clone https://LuciferSniper:$1@github.com/LuciferSniper/$2.git
 cd node
 chmod +x install.sh 
 ./install.sh
